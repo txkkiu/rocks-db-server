b8e1af8f303094f8c2a336e135996d0f52ab6ba2
koek67 <krishnan.koushik@gmail.com>
2017-02-28 17:03:03 -0500

    fixed a dumb coding error. everything back online again

--- src/main/java/http/Application.java
+++ src/main/java/http/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application extends MainController {
+public class Application {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
--- src/main/java/http/MainController.java
+++ src/main/java/http/MainController.java
@@ -3,22 +3,21 @@ package http;
 import models.ModelUtils;
 import models.Response;
 import models.ValueResponse;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import rocksdb.RocksDBFactory;
 import rocksdb.SimpleRocksDB;
 
-@Controller
-public class MainController extends RocksDBFactory {
+@RestController
+public class MainController {
 
-    @RequestMapping(name = "/get_key", method = RequestMethod.GET)
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
     public String get(@RequestParam("db") String db, @RequestParam("key") String key) {
         SimpleRocksDB rocksDB = RocksDBFactory.getDB(db);
         ValueResponse valueResponse = new ValueResponse(rocksDB.get(key));
         return ModelUtils.write(valueResponse);
     }
 
-    @RequestMapping(name = "/set_key", method = RequestMethod.POST)
+    @RequestMapping(value = "/set", method = RequestMethod.POST)
     public String set(@RequestParam("db") String db, @RequestParam("key") String key, @RequestParam("value") String value) {
         SimpleRocksDB rocksDB = RocksDBFactory.getDB(db);
         Response response = new Response(rocksDB.set(key, value));
